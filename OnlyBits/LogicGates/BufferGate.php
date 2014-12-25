637801b014848a3fd3fa7940aa7fb137b25a9a96
Ricardo Tonet <ribeiro.tonet@gmail.com>
2014-12-25 19:51:21 +0000

    Create Buffer gate and corresponding tests.

---
+++ OnlyBits/LogicGates/BufferGate.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace OnlyBits\LogicGates;
+
+class BufferGate extends LogicGate
+{
+    public function __construct()
+    {
+        parent::__construct(1);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function out()
+    {
+        $this->output = $this->inputs["1"];
+
+        return $this->output;
+    }
+}
