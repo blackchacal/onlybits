d3764d7cc8e21aa071825a04e89f0784db661359
Ricardo Tonet <ribeiro.tonet@gmail.com>
2015-01-02 18:29:40 +0000

    Remove testWireIsReturned(). This test is no longer needed because the wire is no longer returned on connection.

--- tests/OnlyBits/Connectors/WireTest.php
+++ tests/OnlyBits/Connectors/WireTest.php
@@ -15,14 +15,4 @@ class WireTest extends PHPUnit_Framework_TestCase
 
         $wire1->connect($wire2);
     }
-
-    public function testWireIsReturned()
-    {
-        $wire1 = $this->getMockForAbstractClass('OnlyBits\Connectors\WireAbstract');
-        $wire2 = $this->getMockForAbstractClass('OnlyBits\Connectors\WireAbstract');
-
-        $wire = $wire1->connect($wire2);
-
-        $this->assertInstanceOf('OnlyBits\Connectors\WireAbstract', $wire, "No instance of OnlyBits\Connectors\Wire was returned!");
-    }
 }
