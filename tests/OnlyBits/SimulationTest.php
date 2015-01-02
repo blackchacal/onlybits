3a01f00b7da6cc74202503afceed7a9441ddd78d
Ricardo Tonet <ribeiro.tonet@gmail.com>
2015-01-02 18:35:16 +0000

    Add benchmark test for execution time difference between using objects or expressions.

--- tests/OnlyBits/SimulationTest.php
+++ tests/OnlyBits/SimulationTest.php
@@ -33,4 +33,55 @@ class SimulationTest extends PHPUnit_Framework_TestCase
         // Check if light updated state
         $this->assertTrue($light->show());
     }
+
+    public function testSimulationTimeDifferenceBetweenObjectsAndExpressions()
+    {
+        $start_time = microtime(true);
+
+        $a = true;
+        $b = true;
+        $c = true;
+
+        $and = new ANDGate;
+        $or = new ORGate;
+        $wire1 = new LogicWire;
+        $wire2 = new LogicWire;
+        $wire3 = new LogicWire;
+        $wire4 = new LogicWire;
+        $wire5 = new LogicWire;
+
+        $wire1->setValue($a);
+        $wire2->setValue($b);
+        $wire3->setValue($c);
+
+        $and->connect($wire1, 1);
+        $and->connect($wire2, 2);
+        $and->connect($wire4);
+        $or->connect($wire3, 2);
+        $or->connect($wire4, 1);
+        $or->connect($wire5);
+
+        $and->out();
+        $or->out();
+
+        $out = $wire5->getValue();
+
+        $end_time = microtime(true);
+        $bench_1 = $end_time - $start_time;
+
+        $start_time = microtime(true);
+
+        $a = true;
+        $b = true;
+        $c = true;
+
+        $s = ($a and $b) or $c;
+
+        $end_time = microtime(true);
+        $bench_2 = $end_time - $start_time;
+
+        $this->assertEquals($s, $out, "Values don't match!");
+
+        $this->assertGreaterThan($bench_2, $bench_1, "Objects take less time than expression!");
+    }
 }
