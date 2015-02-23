cedba2686fb4870802106b2418e42729c9266fda
Ricardo Tonet <ribeiro.tonet@gmail.com>
2015-02-23 21:29:36 +0000

    Add gulp recipes for browserify, watchify and uglify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,29 @@
-var gulp = require('gulp');
+'use strict';
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+
+var bundler = watchify(browserify('./src/app.js', watchify.args));
+// add any other browserify options or transforms here
+// bundler.transform('brfs');
+
+gulp.task('js', bundle); // so you can run `gulp js` to build the file
+bundler.on('update', bundle); // on any dep update, runs the bundler
+
+function bundle() {
+  return bundler.bundle()
+    // log errors if they happen
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('onlybits.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./')) // writes .map file
+    .pipe(gulp.dest('./dist'));
+}
